a5c369a9ec6e3fbe9674a14359df600215346aa5
Gustavo230007 <sousagusta500@gmail.com>
2025-11-27 15:27:57 -0300

    Add files via upload

---
+++ desafio2.spec.js
@@ -0,0 +1,26 @@
+import { test } from '@playwright/test';
+
+test('go to promo do rafa', async ({ page }) => {
+  await page.goto('https://promosdarafa.com.br/lojas/mercadolivre?tab=coupons');
+
+  await page.locator(".sc-1766af8-0.dxZQct").first().waitFor({timeout: 10000, state: 'visible'})
+
+  const cards = await page.locator(".sc-1766af8-0.dxZQct").all();
+  const buttons = await page.locator(".sc-e7eb2e76-6.hJndBQ").all();
+  const descriptions = await page.locator(".sc-1766af8-4.ckmswG").all();
+
+
+  const result = [];
+
+  const total = Math.min(cards.length, buttons.length, descriptions.length);
+
+  for (let i = 0; i < total; i++) {
+    result.push({
+      card: await cards[i].innerText(),
+      button_link: await buttons[i].getAttribute("href"),
+      description: await descriptions[i].innerText()
+    });
+  }
+
+  console.log(result);
+});
